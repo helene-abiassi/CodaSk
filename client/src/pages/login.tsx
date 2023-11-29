b9488fab148d09641d36b1ad318e419c9373d6bf
Thair Orfahli <thair.sy@gmail.com>
2023-11-29 13:40:58 +0100

    post a new quetions works

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -3,7 +3,18 @@ import React from 'react';
 type Props = {};
 
 function Login({}: Props) {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label className="mb-2 block text-sm font-bold text-gray-700">
+            Username
+          </label>
+          <input className="hover:shadow-md" type="text" />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
